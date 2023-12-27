fec1a0432573581d692a605bfc8e68344d8951ed
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-12-27 23:59:12 +0530

    db

--- blog_server/db/connectDB.js
+++ blog_server/db/connectDB.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/blog_mern');
+// mongoose.connect('mongodb://localhost:27017/blog_mern');
 
-mongoose.connection.on('connected', () => {
-    console.log('connected to mongoDB');
-});
-mongoose.connection.on('error', (err) => {
-    console.log('error connected to mongodb:', err);
-})
+// mongoose.connection.on('connected', () => {
+//     console.log('connected to mongoDB');
+// });
+// mongoose.connection.on('error', (err) => {
+//     console.log('error connected to mongodb:', err);
+// });
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect('mongodb://localhost:27017/blog_mern');
+      console.log('Connected to MongoDB');
+    } catch (err) {
+      console.error('Error connecting to MongoDB:', err);
+    }
+  };
+  
+  connectDB();
+
+module.exports = mongoose;
