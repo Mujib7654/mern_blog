ee9e18410155a8d7b6c3c8fcde49415d31d9f596
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-12-31 19:50:36 +0530

    login

--- blog_server/routes/users.js
+++ blog_server/routes/users.js
@@ -1,8 +1,25 @@
 const bcrypt = require('bcryptjs');
 const express = require('express');
+//to upload file
+const path = require('path');
+const multer = require('multer');
+
 const router = express.Router();
 
 const User = require('../models/users');
+
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function(req, file, cb){
+        //cb: callback
+        cb(null, file.fieldname + '-' + Date.now() + '.png');
+    }
+});
+//initialize multer
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 1000000}
+});
 
 //add user
 router.post('/addUser', async(req, res) => {
@@ -67,6 +84,15 @@ router.delete('/deleteUser/:userid', async(req, res) => {
     try {
         const deleteUser = await User.findByIdAndDelete(uid);
         res.status(200).json({'msg': 'user has been deleted successfully'});
+    } catch (error) {
+        res.status(500).json({'error': error});
+    }
+})
+
+//upload image
+router.post('/uploadimage', upload.single('profile_pic'), async(req, res) => {
+    try {
+        res.json({'msg': 'file uploaded successfully'});
     } catch (error) {
         res.status(500).json({'error': error});
     }
